4244862886f8c7eabc85e0b19bab60493882d700
Dhivas Sugumar <dhivassugumar@gmail.com>
2024-07-28 23:32:52 -0400

    Added color system, spacing, radi, and screen breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,16 +5,41 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./packages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
-  },
-  plugins: [],
-};
+    colors: {
+      // Add colorPallette here from theme design system
+    },
+    fontFamily: {
+      // Decide on font families
+    },
+    // Move these values into the theme design system package
+    extend: {
+      spacing: {
+        "sm": "8px",
+        "md": "16px",
+        "l": "24px",
+        "xl": "32px",
+        "xxl": "48px"
+      },
+      borderRadius: {
+        "xs": "2px",
+        "xm": "4px",
+        "md": "6px",
+        "lg": "8px",
+        "xl": "12px",
+        "xxl": "16px",
+        "xxxl": "24px",
+        "full": "9999px"
+      }
+    }
+  }
+}
 export default config;
