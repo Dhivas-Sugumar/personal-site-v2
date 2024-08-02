11013ba676ad3c0e51372ae32545fdc185e76d05
Dhivas Sugumar <dhivassugumar@gmail.com>
2024-08-02 01:59:14 -0400

    Added link to about for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Hero } from "@/design-system";
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Hero />
-    </div>
+    <Link href="/about">
+      <div className="flex items-center justify-center min-h-screen">
+        <Hero />
+      </div>
+    </Link>
   );
 }
