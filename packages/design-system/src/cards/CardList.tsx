c9b9b9bae17fe2b09aa77f81ea2cd2563defd590
Dhivas Sugumar <dhivassugumar@gmail.com>
2024-08-04 02:56:25 -0400

    Created a card list componet

---
+++ packages/design-system/src/cards/CardList.tsx
@@ -0,0 +1,14 @@
+type CardListProps<T> = {
+  cards: T[];
+  CardComponent: React.ComponentType<T>;
+};
+
+export function CardList<T>({ cards, CardComponent }: CardListProps<T>) {
+  return (
+    <div className="flex flex-column">
+      {cards.map((card, index) => (
+        <CardComponent key={index} {...card} />
+      ))}
+    </div>
+  );
+}
