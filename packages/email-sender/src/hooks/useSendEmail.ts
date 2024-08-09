8744ada687628db0b35b36e3c791cc1f9b0abe1d
Dhivas Sugumar <dhivassugumar@gmail.com>
2024-08-08 21:17:58 -0400

    Redid useSendEmail to use gmail

--- packages/email-sender/src/hooks/useSendEmail.ts
+++ packages/email-sender/src/hooks/useSendEmail.ts
@@ -1,17 +1,45 @@
-import { MailtrapClient, Address } from "mailtrap";
+import { createTransport } from "nodemailer";
 
-const client = new MailtrapClient({ token: process.env.MAILTRAP_TOKEN || "" });
+// Create a transporter object
+const transporter = createTransport({
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.PERSONAL_EMAIL,
+    pass: process.env.PERSONAL_EMAIL_APP_PASSWORD,
+  },
+});
+type EmailSendSuccess = {
+  success: true;
+};
+type EmailSendReject = {
+  success: false;
+  error: string;
+};
 
+export type EmailSendResult = EmailSendSuccess | EmailSendReject;
+
+// TODO: Use Success and Reject Type from nodemailer
 export const useSendEmail = async (
   senderName: string,
   senderEmail: string,
   subject: string,
   content: string
-) => {
-  return await client.send({
-    from: { name: senderName, email: senderEmail },
-    to: [{ email: process.env.PERSONAL_EMAIL || "" }],
-    subject,
+): Promise<EmailSendResult> => {
+  const mailOptions = {
+    from: senderEmail,
+    to: process.env.PERSONAL_EMAIL,
+    subject: `${senderName}: ${subject}`,
     text: content,
-  });
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return { success: true };
+  } catch (error) {
+    console.error("Failed to send email:", error);
+    return { success: false, error: "Failed to send email" };
+  }
 };
