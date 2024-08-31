b59b17b1eceff687468feac5e8d818d17e81cb07
Dhivas Sugumar <65351416+Dhivas-Sugumar@users.noreply.github.com>
2024-08-31 10:30:10 -0400

    Created year segmeneted experience list (#36)

--- packages/experience/src/components/experience/ExperienceCardList.tsx
+++ packages/experience/src/components/experience/ExperienceCardList.tsx
@@ -1,12 +1,20 @@
 import { CardList } from "@/design-system/src/cards/CardList";
-import { ExperienceCard } from "@/experience/src/components/ExperienceCard";
-import { experienceCards } from "./Constants";
-
+import exp from "constants";
+import { experienceYears } from "./Constants";
+import { ExperienceYear } from "./ExperienceYear";
 export const ExperienceCardList: React.FC = () => {
   return (
     <div>
       <h2>Experience</h2>
-      <CardList cards={experienceCards} CardComponent={ExperienceCard} />
+      {experienceYears.years.map((experienceYear) => {
+        return (
+          <ExperienceYear
+            year={experienceYear.year}
+            experiences={experienceYear.experiences}
+          />
+        );
+      })}
+      ;
     </div>
   );
 };
