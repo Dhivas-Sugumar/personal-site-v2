63056225dc4505c95e2ea91a0f2a94036b60e288
Dhivas Sugumar <dhivassugumar@gmail.com>
2024-09-07 21:16:36 -0400

    Fixed the text

--- packages/about-me/src/about-me-text.tsx
+++ packages/about-me/src/about-me-text.tsx
@@ -4,11 +4,11 @@ export const AboutMeText: React.FC = () => {
       <h1>Hiya,</h1>
       <p className="body2 text-xl">
         My passion and curiosity for computers sparked when I first laid eyes on
-        a Dell PC around 2009 at my dad's business. I was determined to have one
-        at home, and after some persistence, my brothers and I finally got our
-        own. I couldn’t resist tearing it apart and putting it back together,
-        and from that moment on, I was hooked—I wanted to understand the
-        language that made these machines tick: computer science.
+        a Dell PC around 2009 at my dad's workplace. I was determined to have
+        one at home, and after some persistence, my brothers and I finally got
+        our own. I couldn’t resist tearing it apart and putting it back
+        together, and from that moment on, I was hooked—I wanted to understand
+        the language that made these machines tick: computer science.
         <br />
         <br />
         This led me to major in Computer Science at Northeastern University.
