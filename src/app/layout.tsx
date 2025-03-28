667da25f1ac7b7e27adee0d8ba54ab95e124b9b7
Dhivas-Sugumar <dhivassugumar@gmail.com>
2025-03-28 17:53:44 -0400

    Added children back in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className="bg-neutral0">
+        {children}
         <ToastProvider />
         <Analytics />
       </body>
